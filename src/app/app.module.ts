95452cbeed15696db6a2c377faefa5aa815c97e3
Waylon <wayloncn@gmail.com>
2022-09-15 15:07:10 -0400

    Add home page   - Created LayoutComponent for site-wide layout   - Created HomePageComponent   - Defined default route and home page route   - Added LayoutComponent to AppComponent   - Added router-outlet projection to LayoutComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
