5a3771b7fa9f85e4aec74d578a0dbfdc52183dfa
Waylon <wayloncn@gmail.com>
2022-09-18 10:47:50 -0400

    added angular material dependency   - ran ng add @angular/material and chose the following options:     - theme: depp purple/amber     - global material typography: yes     - animations: include and enable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MenuPageComponent } from './pages/menu-page/menu-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MenuPageComponent } from './pages/menu-page/menu-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
