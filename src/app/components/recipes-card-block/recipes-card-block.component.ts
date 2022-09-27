f4f8a0f1803f0efa5d86cca0bac733e9073280a5
Waylon <wayloncn@gmail.com>
2022-09-27 12:15:27 -0400

    made recipe card block responsive

--- src/app/components/recipes-card-block/recipes-card-block.component.ts
+++ src/app/components/recipes-card-block/recipes-card-block.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IRecette } from 'models/recipe.model';
+import { RecipeService } from 'services/recipe.service';
 
 @Component({
   selector: 'app-recipes-card-block',
   templateUrl: './recipes-card-block.component.html',
-  styleUrls: ['./recipes-card-block.component.scss']
+  styleUrls: ['./recipes-card-block.component.scss'],
 })
 export class RecipesCardBlockComponent implements OnInit {
+  recipes: IRecette[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.recipeService.recipes.subscribe((recipes) => {
+      this.recipes = recipes;
+    });
   }
-
 }
