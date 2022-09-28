6dd158620e7b6ecef26f489a54c5c400e927d921
Jennifer Pierre <shelynx@msn.com>
2022-09-28 10:28:42 -0400

    ajuster calendrier

--- src/app/components/calendar-block/calendar-block.component.ts
+++ src/app/components/calendar-block/calendar-block.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { IRecette } from 'models/recipe.model';
+import { RecipeService } from 'services/recipe.service';
 
 @Component({
   selector: 'app-calendar-block',
@@ -8,7 +10,9 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class CalendarBlockComponent implements OnInit {
   panelOpenState = false;
   selected = new Date();
+  recipes: IRecette[] = [];
 
+//size for mobile
   gridCols = 1;
 
   calendarTileColSpan = 1;
@@ -26,10 +30,13 @@ export class CalendarBlockComponent implements OnInit {
   menuTileColSpan = 1;
   menuTileRowSpan = 1;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {
+  ngOnInit():void {
     this.handleResize();
+    this.recipeService.recipes.subscribe((recipes)=>{
+      this.recipes = recipes;
+    });
   }
 
   @HostListener('window:resize')
@@ -39,6 +46,7 @@ export class CalendarBlockComponent implements OnInit {
     const windowSize = window.innerWidth;
     console.log(windowSize);
 
+//size for mobile/tablet
     if (windowSize < 700) {
       this.gridCols = 1;
 
@@ -58,22 +66,23 @@ export class CalendarBlockComponent implements OnInit {
       this.menuTileColSpan = 1;
       this.menuTileRowSpan = 1;
     } else {
+      //size for desktop
       this.gridCols = 4;
 
-      // Calendar grid tile
-      this.calendarTileColSpan = 2;
+      // step 1 - Calendar grid tile
+      this.calendarTileColSpan = 3;
       this.calendarTileRowSpan = 1;
 
-      // Day/Week grid tile
+      // step 2 - Day/Week grid tile
       this.dayWeekTileColSpan = 2;
       this.dayWeekTileRowSpan = 1;
 
-      // 'Add recipe button' grid tile
+      // step 3 - 'Add recipe button' grid tile
       this.addRecipeBtnTileColSpan = 1;
       this.addRecipeBtnTileRowSpan = 1;
 
-      // Menu grid tile
-      this.menuTileColSpan = 3;
+      // step 4 - Menu Result grid tile
+      this.menuTileColSpan = 2;
       this.menuTileRowSpan = 1;
     }
   }
