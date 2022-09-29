79a15941640b64406f21e299957469b2d26f57fb
Waylon <wayloncn@gmail.com>
2022-09-29 14:52:32 -0400

    made some changes to AuthService   - separated logic into login/logout   - redirect to home on logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { firstValueFrom, Subject } from 'rxjs';
+
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-
 import firebase from 'firebase/compat';
+
 import { IUser } from 'models/user.model';
-import { firstValueFrom, Observable, Subject, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -18,38 +20,52 @@ export class AuthService {
   private userCollection: AngularFirestoreCollection<IUser>;
   appUser: Subject<IUser | undefined>;
 
-  constructor(private auth: AngularFireAuth, private afs: AngularFirestore) {
+  constructor(
+    private auth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
     this.userCollection = this.afs.collection<IUser>('users');
     this.appUser = new Subject<IUser | undefined>();
 
     this.auth.user.subscribe(async (user) => {
-      this.firebaseUser = user;
-      await this.setUser();
+      if (user) {
+        this.firebaseUser = user;
+        await this.login();
+      } else {
+        this.logout();
+      }
     });
   }
 
-  async setUser() {
-    console.log('in setUser');
+  async login() {
     if (this.firebaseUser) {
       const registeredUser = await firstValueFrom(
         this.userCollection.doc(this.firebaseUser.uid).get()
       );
 
-      if (!registeredUser.exists) {
+      if (registeredUser.exists) {
+        this._appUser = registeredUser.data();
+      } else {
         // newly registered user
-        console.log('creating new user');
-
-        const user = {
+        const newUser = {
           uid: this.firebaseUser.uid,
           nomAfficher: '',
           email: this.firebaseUser.email!,
           role: 'M',
         };
 
-        await this.userCollection.doc(user.uid).set(user);
+        await this.userCollection.doc(newUser.uid).set(newUser);
+        this._appUser = newUser;
       }
-
-      this.appUser.next(registeredUser.data());
     }
+
+    this.appUser.next(this._appUser);
+  }
+
+  logout() {
+    this._appUser = undefined;
+    this.appUser.next(this._appUser);
+    this.router.navigate(['/home']);
   }
 }
