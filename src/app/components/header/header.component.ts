53d30c2db659def81d9e504befd81cbf92743bd2
Jennifer Pierre <shelynx@msn.com>
2022-10-05 18:32:46 -0400

    adjustement du sidenav et mat-toolbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'services/auth.service';
-import {MatSidenav} from 'angular/material/sidenav'
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +8,8 @@ import {MatSidenav} from 'angular/material/sidenav'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
+  @ViewChild(MatSidenavModule)
+  sidenav!: MatSidenavModule;
   
   constructor(private auth: AuthService) {}
   ngOnInit(): void {}
