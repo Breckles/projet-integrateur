b6c24f609a14e5696f81ff9acfbe51066d14c27a
Jennifer Pierre <shelynx@msn.com>
2022-10-06 22:24:50 -0400

    tried  user form pour nomafficher

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { AuthService } from 'services/auth.service';
 
 import { IUser } from 'models/user.model';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
@@ -12,14 +13,39 @@ import { IUser } from 'models/user.model';
 })
 export class ProfilePageComponent implements OnInit, OnDestroy {
   user: IUser | undefined;
+  myForm!: FormGroup
+  @Input()
+  userToModify: IUser | null = null;
   private userSubscription: Subscription | null = null;
 
   constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
+    const userFormList = new FormArray<any>([]);
+
+if(!this.userToModify){
+  this.myForm = new FormGroup({
+    nomAfficher: new FormControl<string>('', Validators.nullValidator)
+  })
+}else{
+ this.myForm = new FormGroup({
+  nomAfficher: new FormControl<string>(
+    this.userToModify.nomAfficher, Validators.nullValidator
+  )
+ })
+}
+
+
+
     this.auth.getUser().then((user) => {
       this.user = user;
     });
+  }
+
+  onSubmit(){
+      const userForm ={
+        nomAfficher: this.myForm.value.nomAfficher as string
+      }
   }
 
   ngOnDestroy(): void {
